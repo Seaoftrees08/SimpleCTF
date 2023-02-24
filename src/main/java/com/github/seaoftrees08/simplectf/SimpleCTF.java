49783dbb3e70c465734fe89a8a3209d7008ab4a3
Seaoftrees08 <tasuzuya@gmail.com>
2023-02-24 17:12:43 +0900

    フルスクラッチはじめ

---
+++ src/main/java/com/github/seaoftrees08/simplectf/SimpleCTF.java
@@ -0,0 +1,18 @@
+package com.github.seaoftrees08.simplectf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleCTF extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("hi");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
